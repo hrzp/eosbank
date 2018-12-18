28097924f63de5570559ee82de883da7cd716d23
Hamid Reza Zare Pour <hamidreza.num1@gmail.com>
2018-12-18 12:27:38 +0330

    add set_config function

--- eosbank.cpp
+++ eosbank.cpp
@@ -19,15 +19,49 @@ void bank::init()
     eosio_assert( iterator != _config.end(),  "CONFIG NOT SET");
 
     const auto& item = _config.get( 0 );
+    bank::eosPrice = item.eosPrice;
     // TODO: check config
 }
 
 
-void is_pausing()
+void bank::is_pausing()
 {
     config _config(_code, _code.value);
     const auto& config = _config.get( 0 );
     eosio_assert( config.pause == false,  "CONTRACT PAUSED.");
+}
+
+void bank::set_config(  bool    pause,
+                        name    oracleAddress,
+                        name    liquidatorAdd,
+                        float   eosPrice,
+                        float   depositRate,
+                        uint32_t liquidationDuration)
+{
+    require_auth(get_self());
+    config _config(_code, _code.value);
+    auto iterator = _config.find( 0 );
+    if( iterator == _config.end() ) {
+        _config.emplace(_code, [&]( auto& row ) {
+            row.key = 0;
+            row.pause = pause;
+            row.oracleAddress = oracleAddress;
+            row.liquidatorAdd = liquidatorAdd;
+            row.eosPrice = eosPrice;
+            row.depositRate = depositRate;
+            row.liquidationDuration = liquidationDuration;
+        });
+    }
+    else {
+        _config.modify(iterator, _code, [&]( auto& row ) {
+            row.pause = pause;
+            row.oracleAddress = oracleAddress;
+            row.liquidatorAdd = liquidatorAdd;
+            row.eosPrice = eosPrice;
+            row.depositRate = depositRate;
+            row.liquidationDuration = liquidationDuration;
+        });
+    }
 }
 
 } /// namespace eosbank

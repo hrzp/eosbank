e6f8e9b2f382b28d0014130748f01b2a2b992ddb
Hamid Reza Zare Pour <hamidreza.num1@gmail.com>
2018-12-18 11:52:25 +0330

    add config table and init, is_pausing functions

--- eosbank.cpp
+++ eosbank.cpp
@@ -3,4 +3,31 @@
 
 namespace eosbank {
 
+eosbank(  name        receiver,
+          name        code,
+          datastream  <const char*> ds):
+          contract( receiver, code, ds )
+{
+  init();
+}
+
+
+void bank::init()
+{
+    config _config(_code, _code.value);
+    auto iterator = _config.find( 0 );
+    eosio_assert( iterator != _config.end(),  "CONFIG NOT SET");
+
+    const auto& item = _config.get( 0 );
+    // TODO: check config
+}
+
+
+void is_pausing()
+{
+    config _config(_code, _code.value);
+    const auto& config = _config.get( 0 );
+    eosio_assert( config.pause == false,  "CONTRACT PAUSED.");
+}
+
 } /// namespace eosbank
--- include/eosbank/eosbank.hpp
+++ include/eosbank/eosbank.hpp
@@ -8,12 +8,27 @@ namespace eosbank {
 
    using std::string;
 
-   class [[eosio::contract("eosbank")]] eosbankcontract : public contract {
+   class [[eosio::contract("eosbank")]] bank : public contract {
       public:
          using contract::contract;
 
       private:
+         struct [[eosio::table]] config_table {
+            uint64_t key;
+            bool     pause;
+            name     oracleAddress;
+            name     liquidatorAdd;
+            float    eosPrice;
+            float    depositRate;
+            uint64_t liquidationDuration; // better to be in uint32
 
+            uint64_t primary_key() const { return key; }
+         };
+
+         typedef eosio::multi_index< "config"_n, config_table > config;
+
+         void init();
+         void is_pausing();
    };
 
 } /// namespace eosbank

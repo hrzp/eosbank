c37a7028adbfa33b2d5ca2a5e85a9596c32da1cd
Hamid Reza Zare Pour <hamidreza.num1@gmail.com>
2018-12-22 14:52:02 +0330

    add some structers and withdraw action

--- include/liquidator/liquidator.hpp
+++ include/liquidator/liquidator.hpp
@@ -25,6 +25,59 @@ namespace eosio {
       private:
          #define MYT_SYMBOL symbol( "MYT", 4 )
          #define EOS_SYMBOL symbol( "EOS", 4 )
+         const char *INVALID_ADDRESS =          "INVALID_ADDRESS";
+         const char *ONLY_ETHER_BANK =          "ONLY_ETHER_BANK";
+         const char *NO_DEPOSIT =               "NO_DEPOSIT";
+         const char *INVALID_AMOUNT =           "INVALID_AMOUNT";
+         const char *NOT_ACTIVE_LIQUIDATION =   "NOT_ACTIVE_LIQUIDATION";
+         const char *OPEN_LIQUIDATION =         "OPEN_LIQUIDATION";
+         const char *NO_BID =                   "NO_BID";
+         const char *INADEQUATE_BIDDING =       "INADEQUATE_BIDDING";
+         const char *INSUFFICIENT_FUNDS =       "INSUFFICIENT_FUNDS";
+
+
+         struct [[eosio::table]] config_table {
+            uint64_t id;
+            bool     pause;
+            name     oracleAddress;
+            name     liquidatorAdd;
+            float    eosPrice;
+            float    depositRate;
+            uint64_t liquidationDuration;
+
+            uint64_t primary_key() const { return id; }
+         };
+
+
+         struct [[eosio::table]] liquidation {
+            uint64_t       loanid;
+            asset          collateral;
+            asset          amount;
+            uint32_t       start_time;
+            uint32_t       end_time;
+            asset          best_bid;
+            name           best_bider;
+            uint8_t        state;
+
+            uint64_t primary_key() const { return loanid; }
+         };
+
+         struct [[eosio::table]] deposit {
+            name     user;
+            asset    amount;
+
+            uint64_t primary_key() const { return user.value; }
+         };
+
+         enum liqudiation_stats {
+           ACTIVE = 0,
+           FINISHED,
+           FAILED
+         }
+
+         typedef eosio::multi_index< "config"_n, config_table > config;
+         typedef eosio::multi_index< "deposits"_n, deposits_table > deposits;
+         typedef eosio::multi_index< "trustfund"_n, trust_fund > trustfund;
 
          void init();
          void is_pausing();
--- src/liquidator.cpp
+++ src/liquidator.cpp
@@ -23,6 +23,32 @@ void liquidator::is_pausing()
 }
 
 
+void liquidator::withdraw( name user)
+{
+	require_auth( user );
+	is_pausing();
+
+    deposits deposit( _code, _code.value );
+
+    const auto& item = deposit.get( user.value, NO_DEPOSIT );
+    eosio_assert ( item.amount.amount > 0, NO_DEPOSIT);
+
+    auto iterator = deposit.find( user.value );
+    deposit.modify(iterator, _code, [&]( auto& row ) {
+        row.amount = asset(0, MYT_SYMBOL);
+    });
+
+    // transfer eos for buyer
+    action(
+        permission_level{ get_self(),"active"_n },
+        "myteostoken"_n, // TODO: declare in config
+        "transfer"_n,
+        std::make_tuple( get_self(), user, amount, std::string("WITHDRAW") )
+    ).send();
+
+}
+
+
 } /// namespace eosliquidator
 
 
